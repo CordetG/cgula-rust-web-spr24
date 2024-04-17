f03de1e4d6c409a1d60bbff76c5930ac047ae013
Cordet Gula <cgula@pdx.edu>
2024-04-16 20:49:13 -0700

    add id_check ch.1 sec 1.2

--- dev-examples/hello/src/main.rs
+++ dev-examples/hello/src/main.rs
@@ -1,3 +1,18 @@
+use std::todo;
+// For empty ID checking
+#[allow(unused_imports)]
+use std::{fmt::Error, io::ErrorKind, process::id};
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[allow(dead_code)]
+fn err_id_check() {
+    todo!("Check for empty ID not fully implemented");
+    /* match id.is_empty() {
+        false => Ok(QuestionId(id.to_string())),
+        true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+    }
+    */
 }
