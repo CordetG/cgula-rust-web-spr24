29c0acab53bd7b69314aeeac8ccb0f7dbe72851b
Cordet Gula <cgula@pdx.edu>
2024-04-19 23:23:49 -0700

    reqwest example

--- dev-examples/hello/src/main.rs
+++ dev-examples/hello/src/main.rs
@@ -1,19 +1,35 @@
 use std::todo;
-// For empty ID checking
+#[allow(unused_imports)]
 use std::net::{TcpListener, TcpStream};
+// For empty ID checking
 #[allow(unused_imports)]
 use std::{fmt::Error, io::ErrorKind, process::id};
+use std::collections::HashMap;
 
 #[allow(unused_variables)]
+#[allow(dead_code)]
 fn handle_client(stream: TcpStream) {
     todo!();
 }
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:80")?;
+// ch_01/minimal_reqwest/src/main.rs
+// https://github.com/Rust-Web-Development/code/tree/main/ch_01/minimal_reqwest
+ 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // http example
+    /* let listener = TcpListener::bind("127.0.0.1:80")?;
     for stream in listener.incoming() {
         handle_client(stream?);
     }
+    Ok(())*/
+
+    // reqwest with async/await
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
     Ok(())
 }
 
