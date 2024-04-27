113e84bb494d62b4d9b6cab630e658ca6d07db53
CordetG <cgula@pdx.edu>
2024-04-26 23:12:55 -0700

    todo: figure out axum version of warp::filter

--- ch4-web/src/main.rs
+++ ch4-web/src/main.rs
@@ -1,14 +1,16 @@
 // Implementation for ch4 continued from ch3/ to set up RESTful API
 
-#![allow(unused_imports, dead_code, unused_must_use)]
+#![allow(unused_imports, dead_code, unused_must_use, unused_variables)]
 use axum::extract::{self, path, Extension, Path, State};
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
-    routing::{get, post},
+    routing::{any, get, post},
     Json, Router,
 };
 use axum_macros::debug_handler;
+use tower_http::compression::CompressionLayer;
+use tower_http::services::{ServeDir, ServeFile};
 
 use headers::ContentType;
 use serde::{Deserialize, Serialize};
@@ -32,7 +34,7 @@ use std::str::FromStr;
 /// * `tags`: The `tags` field in the `Question` struct is an `Option` that contains a vector of
 /// strings. This means that the `tags` field can either be `Some` with a vector of strings or `None`.
 /// It allows for flexibility in cases where a question may or may not have a value.
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Question {
     id: QuestionId,
     title: String,
@@ -40,7 +42,7 @@ pub struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Hash)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
 struct QuestionId(String);
 
 /// The `impl Question { ... }` block is implementing a method named `new` for the
@@ -93,6 +95,116 @@ impl FromStr for QuestionId {
     }
 }
 
+// Implementing Axum 'IntoResponse' from shuttle.rs but with the Serialized Question
+pub enum ApiResponse {
+    OK,
+    Created,
+    JsonData(Question),
+}
+
+// To return a result, implement an error type
+pub enum ApiError {
+    NotFound,
+    NotImplemented,
+    Failed,
+}
+
+/// The `impl IntoResponse for ApiResponse` block is implementing the `IntoResponse` trait for the
+/// `ApiResponse` enum. This trait allows instances of the `ApiResponse` enum to be converted into an
+/// HTTP response.
+impl IntoResponse for ApiResponse {
+    /// The function `into_response` converts an enum variant into a corresponding HTTP response.
+    ///
+    /// Returns:
+    ///
+    /// The `into_response` function is returning a `Response` object based on the variant of the enum
+    /// `Self`. Depending on the variant, it will create and return a response with the corresponding
+    /// status code and data.
+    fn into_response(self) -> Response {
+        match self {
+            Self::OK => (StatusCode::OK).into_response(),
+            Self::Created => (StatusCode::CREATED).into_response(),
+            Self::JsonData(data) => (StatusCode::OK, Json(data)).into_response(),
+        }
+    }
+}
+
+/// The `impl IntoResponse for ApiError` block is implementing the `IntoResponse` trait for the
+/// `ApiError` enum. This trait allows instances of the `ApiError` enum to be converted into an HTTP
+/// response.
+impl IntoResponse for ApiError {
+    /// The function `into_response` converts enum variants into corresponding HTTP responses.
+    ///
+    /// Returns:
+    ///
+    /// A `Response` object is being returned based on the variant of the enum `self`. The
+    /// `into_response` method is being called on the tuple `(StatusCode, &str)` to convert it into a
+    /// `Response` object.
+    fn into_response(self) -> Response {
+        match self {
+            Self::NotFound => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+            Self::NotImplemented => {
+                (StatusCode::NOT_IMPLEMENTED, "501 Not Implemented").into_response()
+            }
+            Self::Failed => {
+                (StatusCode::EXPECTATION_FAILED, "417 Expectation Failed").into_response()
+            }
+        }
+    }
+}
+
+/// The function `get_questions` asynchronously retrieves a question and returns a result indicating
+/// success or failure.
+///
+/// Returns:
+///
+/// The function `get_questions()` returns a `Result` enum with either an `ApiResponse` or an
+/// `ApiError`. In this specific case, if the parsing of the question ID to an `i32` is successful, it
+/// will return `Ok(ApiResponse::JsonData(question))`, where `question` is an instance of the `Question`
+/// struct. If the parsing fails, it will return an INvalidInput ApiError.
+#[debug_handler]
+async fn get_questions() -> Result<ApiResponse, ApiError> {
+    let question: Question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+    match question.id.0.parse::<i32>() {
+        Err(_) => Err(ApiError::NotFound),
+        Ok(_) => Ok(ApiResponse::JsonData(question)),
+    }
+}
+
+/// The function `init_router` sets up a web server using Axum in Rust, listens on port 3000, and makes
+/// an async request to https://httpbin.org/ip using reqwest.
+///
+/// Returns:
+///
+/// The `init_router` function returns a `Result<(), Box<dyn std::error::Error>>`. This means that it
+/// can either return `Ok(())` indicating that the function executed successfully without any errors, or
+/// it can return an `Err` containing a boxed error trait object that implements the `std::error::Error`
+/// trait in case of any errors occurring during the execution of the function.
+async fn init_router() -> Result<(), Box<dyn std::error::Error>> {
+    let localhost: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+    let socket_addr: SocketAddrV4 = SocketAddrV4::new(localhost, 3040);
+
+    let http_server: Router = Router::new().route("/questions", get(get_questions));
+    // run with hyper, listening globally on port 3080
+    let listener: tokio::net::TcpListener =
+        tokio::net::TcpListener::bind(socket_addr).await.unwrap();
+    tracing::debug!("serving {}", listener.local_addr().unwrap());
+    axum::serve(listener, http_server).await.unwrap();
+
+    // reqwest with async/await
+    let resp: HashMap<String, String> = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
+
 // Section 4.2 -- Creating a 'store' for the questions
 /// The `Store` struct contains a collection of questions stored in a HashMap with `QuestionId` keys and
 /// `Question` values.
@@ -102,6 +214,7 @@ impl FromStr for QuestionId {
 /// * `questions`: The `questions` property in the `Store` struct is a HashMap that stores `Question`
 /// objects with a key of type `QuestionId`. This allows you to efficiently store and retrieve questions
 /// based on their unique identifiers.
+#[derive(Clone)]
 struct Store {
     questions: HashMap<QuestionId, Question>,
 }
@@ -109,26 +222,17 @@ struct Store {
 impl Store {
     fn new() -> Self {
         Store {
-            questions: HashMap::new(),
+            questions: Self::init(),
         }
     }
 
-    fn init(self) -> Self {
-        let question: Question = Question::new(
-            QuestionId::from_str("1").expect("Id not set"),
-            "How?".to_string(),
-            "Please help!".to_string(),
-            Some(vec!["general".to_string()]),
-        );
-        self.add_question(question)
-    }
-
-    fn add_question(mut self, question: Question) -> Self {
-        self.questions.insert(question.id.clone(), question);
-        self
+    fn init() -> HashMap<QuestionId, Question> {
+        let file: &str = include_str!("../questions.json");
+        serde_json::from_str(file).expect("can't read questions.json")
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let store = Store::new();
 }
