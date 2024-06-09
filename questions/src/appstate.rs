fdefc44c5a886b3fc40fd10845c361405fa14e79
Cordet Gula <cgula@pdx.edu>
2024-06-08 17:04:04 -0700

    create an appstate

--- questions/src/appstate.rs
+++ questions/src/appstate.rs
@@ -1,1 +1,15 @@
+use crate::*;
 
+pub struct AppState {
+    pub store: Store,
+}
+
+pub type SharedAppState = Arc<RwLock<AppState>>;
+
+pub type HandlerAppState = State<SharedAppState>;
+
+impl AppState {
+    pub fn new(store: Store) -> Self {
+        Self { store }
+    }
+}
