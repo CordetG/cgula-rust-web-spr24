df3f0a10ecfe13fca08e8b8f26b411363dc790ab
Cordet Gula <cgula@pdx.edu>
2024-06-12 01:23:24 -0700

    add keys

--- backend/src/appstate.rs
+++ backend/src/appstate.rs
@@ -1,7 +1,10 @@
+use crate::auth::JwtKeys;
 use crate::*;
 
 pub struct AppState {
     pub store: Store,
+    pub jwt_keys: JwtKeys,
+    pub reg_key: String,
 }
 
 pub type SharedAppState = Arc<RwLock<AppState>>;
@@ -9,7 +12,11 @@ pub type SharedAppState = Arc<RwLock<AppState>>;
 pub type HandlerAppState = State<SharedAppState>;
 
 impl AppState {
-    pub fn new(store: Store) -> Self {
-        Self { store }
+    pub fn new(store: Store, jwt_keys: JwtKeys, reg_key: String) -> Self {
+        Self {
+            store,
+            jwt_keys,
+            reg_key,
+        }
     }
 }
